8881109d8b229795345fb5bdadc587565884a841
Billy-J-Joseph <68674982+Billy-J-Joseph@users.noreply.github.com>
2022-10-01 12:03:44 +0800

    Add project files.

---
+++ mbaCrixalis/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace mbaCrixalis;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ mbaCrixalis/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mbaCrixalis;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
